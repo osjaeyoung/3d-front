d186a5fae50d1e40364739bdd55eae9b6ac92d0d
Sangkun-svg <gims4752@gmail.com>
2024-10-26 22:51:09 +0900

    build : change server url according exec env

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,10 @@ const nextConfig = {
     return [
       {
         source: "/proxy/:path*",
-        destination: "http://3.38.72.210:3000/:path*",
+        destination:
+          process.env.NEXT_PUBLIC_RUNNING_ENV === "development"
+            ? "http://3.38.72.210:3000/:path*"
+            : "http://3.38.72.210:4000/:path*",
       },
     ];
   },
