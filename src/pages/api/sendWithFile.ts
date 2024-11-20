7eda723a8edcaa432e523b17a723522889b722c4
Sangkun-svg <gims4752@gmail.com>
2024-11-21 01:48:38 +0900

    fix: scaleup bodyparser size limit

--- src/pages/api/sendWithFile.ts
+++ src/pages/api/sendWithFile.ts
@@ -6,7 +6,7 @@ import type { ContactType, MailOptionType } from "@/interface/Mail";
 export const config = {
   api: {
     bodyParser: {
-      sizeLimit: "10mb",
+      sizeLimit: "50mb",
     },
   },
 };
